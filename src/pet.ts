3888f7fab598c41e3b807756d86a2ba5bbf2475d
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-05-17 21:59:35 +0200

    Fix pet file and create petinfo type

--- src/pet.ts
+++ src/pet.ts
@@ -1,7 +1,24 @@
-export type Pet = {
+export type PetInfo = {
   id: string;
   name: string;
   breed: string;
   isAdopted: boolean;
   owner: string;
 };
+
+export class Pet implements PetInfo {
+  static generateId() {
+    return Math.trunc(Math.random() * 10_000).toString();
+  }
+
+  id: string;
+  breed: string;
+  isAdopted: boolean;
+  constructor(public name: string, breed: string, public owner: string) {
+    this.id = Pet.generateId();
+    this.name = name;
+    this.breed = breed;
+    this.isAdopted = false;
+    this.owner = owner;
+  }
+}
